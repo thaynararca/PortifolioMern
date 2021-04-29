8a96da17d73ed81555e03b949e375b65b98a3307
thaynararca <thaynaracosta91@gmail.com>
2021-04-29 19:15:26 -0300

    commit inicial

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Inicio from './pages/client/painel';
+
+function App() {
+    return ( 
+      <div className = "App">
+        {/* <header className = "App-header" >
+        <h1> CURSO BÁSICO DE NODEJS COM REACTJS </h1> 
+        </header>  */}
+        <Inicio />
+        </div>
+    );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
+const routes = require('./src/routes')
+
+const app = express();
+const port = process.env.PORT || 5005;
+
+mongoose.connect(process.env.MONGO_URL ||'mongodb://localhost:27017/curso', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+}, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('MongoDB CONECTADO com sucesso!');
+    }
+});
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+};
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(routes);
+
+app.listen(port, function() {
+    console.log(`Server runing on port ${port}`);
+});
